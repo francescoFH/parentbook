c37660a70f3c44fee10d36f5598a34910aa325a1
francescoFH <francescofacca@gmail.com>
2021-09-28 15:54:46 +0100

    - Add faker gem - Inside the seeds.rb file create methods to generate User, Category and Post records inside the development database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def seed_users
+  user_id = 0
+  10.times do
+    User.create(
+      name: "test#{user_id}",
+      email: "test#{user_id}@test.com",
+      password: '123456',
+      password_confirmation: '123456'
+    )
+    user_id = user_id + 1
+  end
+end
+
+
+def seed_categories
+  cat1 = ['a', 'b', 'c', 'd']
+  cat2 = ['e', 'f', 'g', 'h']
+  cat3 = ['i', 'j', 'k', 'l']
+
+  cat1.each do |name|
+    Category.create(branch: 'cat1', name: name)
+  end
+
+  cat2.each do |name|
+    Category.create(branch: 'cat2', name: name)
+  end
+
+  cat3.each do |name|
+    Category.create(branch: 'cat3', name: name)
+  end
+end
+
+def seed_posts
+  categories = Category.all
+
+  categories.each do |category|
+    5.times do
+      Post.create(
+        title: Faker::Lorem.sentences[0],
+        content: Faker::Lorem.sentences[0],
+        user_id: rand(1..9),
+        category_id: category.id
+      )
+    end
+  end
+end
+
+seed_users
+seed_categories
+seed_posts
